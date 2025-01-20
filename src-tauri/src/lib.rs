c322f9b90bd148ad62ed327b13fc1bdfc93e8c24
xydinga <>
2025-01-20 09:27:05 +0800

    fix:修复编译静态资源路径问题

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate rocket;
 
+use tauri::{AppHandle, Manager};
 use tauri_plugin_log::{Target, TargetKind};
 
 mod commands;
@@ -9,21 +10,24 @@ mod services;
 mod utils;
 use crate::commands::{dsl, menu, page, project};
 use crate::services::preview;
-use rocket::fs::{relative, FileServer, NamedFile};
+use rocket::fs::{FileServer, NamedFile};
 use utils::setup;
+use rocket::{ State, Request};
 
 const APP_ERROR_MSG: &str = "error while running qwikpage application";
 
+
 #[catch(404)]
-async fn not_found() -> Option<NamedFile> {
-    // 返回 SPA 的入口文件，例如 index.html
-    NamedFile::open(relative!("./assets/admin/index.html"))
-        .await
-        .ok()
+async fn not_found(req: &Request<'_>) -> Option<NamedFile> {
+    let handle = req.guard::<&State<AppHandle>>().await.unwrap();
+    let resource_dir = handle.path().resource_dir().expect("Failed to get resource directory");
+    let index_path = resource_dir.join("assets").join("admin").join("index.html");
+    NamedFile::open(index_path).await.ok()
 }
 
-fn configure_rocket() -> rocket::Rocket<rocket::Build> {
-    let static_path = relative!("./assets/admin");
+fn configure_rocket(handle: tauri::AppHandle) -> rocket::Rocket<rocket::Build> {
+    let resource_dir = handle.path().resource_dir().expect("Failed to get resource directory");
+    let admin_path = resource_dir.join("assets").join("admin");
     rocket::build()
         .mount(
             "/api",
@@ -34,8 +38,9 @@ fn configure_rocket() -> rocket::Rocket<rocket::Build> {
                 preview::get_page_detail
             ],
         )
-        .mount("/", FileServer::from(static_path))
+        .mount("/", FileServer::from(admin_path))
         .register("/", catchers![not_found])
+        .manage(handle) // 将 AppHandle 注入 Rocket 状态
 }
 
 pub fn run() {
@@ -74,10 +79,12 @@ pub fn run() {
         ])
         .setup(|app| {
             setup::init(app)?;
+            let handle = app.handle().clone(); // 克隆 AppHandle
             // mount the rocket instance
             tauri::async_runtime::spawn(async move {
-                let _rocket = configure_rocket().launch().await;
-            });
+                let rocket = configure_rocket(handle);
+                let _ = rocket.launch().await;
+            }); 
             Ok(())
         })
         .run(tauri::generate_context!())
