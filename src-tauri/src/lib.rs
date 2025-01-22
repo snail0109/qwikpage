f247c67cfde2a54708ae3267c023b4cf91ac5695
xydinga <>
2025-01-22 15:28:09 +0800

    feat: 加入单实例插件确保 Tauri 应用程序在同一时间只运行单个实例。

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,53 +1,24 @@
 #[macro_use]
 extern crate rocket;
 
-use tauri::{AppHandle, Manager};
 use tauri_plugin_log::{Target, TargetKind};
 
 mod commands;
+mod core;
 mod models;
 mod services;
 mod utils;
-mod core;
 use crate::{
-    commands::{dsl, menu, page, project, config},
+    commands::{config, dsl, menu, page, project},
+    core::setup,
     services::preview,
-    core::setup
 };
-use rocket::fs::{FileServer, NamedFile};
-use rocket::{ State, Request};
 
 const APP_ERROR_MSG: &str = "error while running qwikpage application";
 
-
-#[catch(404)]
-async fn not_found(req: &Request<'_>) -> Option<NamedFile> {
-    let handle = req.guard::<&State<AppHandle>>().await.unwrap();
-    let resource_dir = handle.path().resource_dir().expect("Failed to get resource directory");
-    let index_path = resource_dir.join("assets").join("admin").join("index.html");
-    NamedFile::open(index_path).await.ok()
-}
-
-fn configure_rocket(handle: tauri::AppHandle) -> rocket::Rocket<rocket::Build> {
-    let resource_dir = handle.path().resource_dir().expect("Failed to get resource directory");
-    let admin_path = resource_dir.join("assets").join("admin");
-    rocket::build()
-        .mount(
-            "/api",
-            routes![
-                preview::get_project_detail,
-                preview::get_project_menus,
-                preview::get_menu_detail,
-                preview::get_page_detail
-            ],
-        )
-        .mount("/", FileServer::from(admin_path))
-        .register("/", catchers![not_found])
-        .manage(handle) // 将 AppHandle 注入 Rocket 状态
-}
-
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|_, _, _| {}))
         .plugin(
             tauri_plugin_log::Builder::new()
                 .targets([
@@ -61,6 +32,17 @@ pub fn run() {
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
+        .setup(|app| {
+            info!("============== Start App ==============");
+            setup::init(app)?;
+            let handle = app.handle().clone(); // 克隆 AppHandle
+                                               // mount the rocket instance
+            tauri::async_runtime::spawn(async move {
+                let rocket = preview::configure_rocket(handle);
+                let _ = rocket.launch().await;
+            });
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![
             project::get_project_list,
             project::add_project,
@@ -83,21 +65,6 @@ pub fn run() {
             config::set_theme,
             config::get_app_conf,
         ])
-        .setup(|app| {
-            // .setup(|app|
-        //     // /Users/**/Library/Application Support/com.qwikpage.iwhalecloud
-        //     let app_data_dir = app.path().app_data_dir().unwrap();
-        //     create_dir_all(app_data_dir.clone()).expect("Problem creating App directory!");
-        // )
-            setup::init(app)?;
-            let handle = app.handle().clone(); // 克隆 AppHandle
-            // mount the rocket instance
-            tauri::async_runtime::spawn(async move {
-                let rocket = configure_rocket(handle);
-                let _ = rocket.launch().await;
-            }); 
-            Ok(())
-        })
         .run(tauri::generate_context!())
         .expect(APP_ERROR_MSG);
 }
--- src-tauri/src/services/preview.rs
+++ src-tauri/src/services/preview.rs
@@ -1,12 +1,43 @@
-use crate::commands::{project, menu, page};
-use crate::{
-    models::project::Project, 
-    models::menu::Menu,
-    models::page::Page,
-};
+use crate::commands::{menu, page, project};
+use crate::{models::menu::Menu, models::page::Page, models::project::Project};
 use anyhow::Result;
+use rocket::fs::{FileServer, NamedFile};
 use rocket::http::Status;
 use rocket::serde::json::Json;
+use rocket::{Request, State};
+use tauri::{AppHandle, Manager};
+
+#[catch(404)]
+pub async fn not_found(req: &Request<'_>) -> Option<NamedFile> {
+    let handle = req.guard::<&State<AppHandle>>().await.unwrap();
+    let resource_dir = handle
+        .path()
+        .resource_dir()
+        .expect("Failed to get resource directory");
+    let index_path = resource_dir.join("assets").join("admin").join("index.html");
+    NamedFile::open(index_path).await.ok()
+}
+
+pub fn configure_rocket(handle: tauri::AppHandle) -> rocket::Rocket<rocket::Build> {
+    let resource_dir = handle
+        .path()
+        .resource_dir()
+        .expect("Failed to get resource directory");
+    let admin_path = resource_dir.join("assets").join("admin");
+    rocket::build()
+        .mount(
+            "/api",
+            routes![
+                get_project_detail,
+                get_project_menus,
+                get_menu_detail,
+                get_page_detail
+            ],
+        )
+        .mount("/", FileServer::from(admin_path))
+        .register("/", catchers![not_found])
+        .manage(handle) // 将 AppHandle 注入 Rocket 状态
+}
 
 #[get("/project/detail/<id>")]
 pub fn get_project_detail(id: String) -> Result<Json<Project>, Status> {
@@ -16,7 +47,6 @@ pub fn get_project_detail(id: String) -> Result<Json<Project>, Status> {
     }
 }
 
-
 #[get("/project/menus/<id>")]
 pub fn get_project_menus(id: String) -> Result<Json<Vec<Menu>>, Status> {
     match menu::get_menu_list(id, None, -1) {
@@ -25,16 +55,13 @@ pub fn get_project_menus(id: String) -> Result<Json<Vec<Menu>>, Status> {
     }
 }
 
-
-
 #[get("/menu/detail/<project_id>/<id>")]
-pub fn get_menu_detail(project_id:String, id: String) -> Result<Json<Menu>, Status> {
+pub fn get_menu_detail(project_id: String, id: String) -> Result<Json<Menu>, Status> {
     match menu::get_menu_detail(id, project_id) {
         Ok(menu) => Ok(Json(menu)),
         Err(_) => Err(Status::InternalServerError),
     }
 }
-
 
 #[get("/page/detail/<id>")]
 pub fn get_page_detail(id: String) -> Result<Json<Page>, Status> {
@@ -43,5 +70,3 @@ pub fn get_page_detail(id: String) -> Result<Json<Page>, Status> {
         Err(_) => Err(Status::InternalServerError),
     }
 }
-
-
