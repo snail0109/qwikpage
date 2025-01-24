08d57f392668b6807583101b97615b9ce60f7b02
xydinga <>
2025-01-24 15:19:11 +0800

    refactor:后端路径处理逻辑重构

--- src-tauri/src/commands/config.rs
+++ src-tauri/src/commands/config.rs
@@ -2,13 +2,12 @@ use std::path::PathBuf;
 
 use tauri::{command, AppHandle, Manager, Runtime};
 use tauri_plugin_opener::OpenerExt;
-use crate::core::conf::AppConf;
+use crate::{core::conf::AppConf, utils::get_app_root_dir};
 
 #[command]
 pub fn open_folder<R: Runtime>(app: AppHandle<R>) -> Result<(), String> {
     
-    let config_dir: PathBuf = app.path().config_dir().unwrap();
-    let root_dir: PathBuf = config_dir.join("qwikpage");
+    let root_dir = get_app_root_dir();
     app.opener()
         .open_path(root_dir.to_string_lossy().to_string(), None::<&str>)
         .map_err(|e| e.to_string())?;
