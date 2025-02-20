d3b8612beb7c673209eef2913a227e126e67c87f
xydinga <>
2025-02-20 17:31:49 +0800

    feat: 更新管理界面静态资源

--- src-tauri/assets/admin/assets/Divider-lo8NyZIO.js
+++
@@ -1,1 +0,0 @@
-import{r,j as o}from"./index-DCpAFO94.js";import{a9 as a}from"./Page-DmTdOxlB.js";import"./index-CdGl5XaA.js";import"./index-C0JJDAxe.js";const p=({config:e},s)=>{const[i,t]=r.useState(!0);return r.useImperativeHandle(s,()=>({show(){t(!0)},hide(){t(!1)}})),i&&o.jsx(a,{style:e.style,...e.props,children:e.props.text})},n=r.forwardRef(p);export{n as default};
--- src-tauri/assets/admin/assets/Title-Dcq2oUK8.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as p}from"./index-DCpAFO94.js";import{T as i}from"./Page-DmTdOxlB.js";import"./index-CdGl5XaA.js";import"./index-C0JJDAxe.js";const a=({config:t},s)=>{const[o,r]=e.useState(!0);return e.useImperativeHandle(s,()=>({show(){r(!0)},hide(){r(!1)}})),o&&p.jsx(i.Title,{style:t.style,...t.props,children:t.props.text})},x=e.forwardRef(a);export{x as default};
