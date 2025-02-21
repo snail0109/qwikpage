0ef242f235d80d0e08b866c083d659308f0bba0d
殷艳 <yin.yan01@iwhalecloud.com>
2025-02-21 18:01:19 +0800

    feat:新增分组名称修改功能

--- frontend/editor/src/pages/home/project/components/ProjectGroup.tsx
+++ frontend/editor/src/pages/home/project/components/ProjectGroup.tsx
@@ -1,14 +1,50 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button, Image, Card } from 'antd';
+import { Button, Input, Card } from 'antd';
 import { EditOutlined, DeleteOutlined, EyeOutlined } from '@ant-design/icons';
 import { message, Modal } from '@/utils/AntdGlobal';
+import { cmd_invoke } from "@/services/cmd_invoke";
 import { IGroup } from '@/types';
 import ProjectCard from "./ProjectCard";
 import styles from './../../index.module.less';
 
-// 页面列表项
-const ProjectGroup = ({ groupItem, onCreate }: { groupItem: IGroup; onCreate: (groupId?: string) => void }) => {
+interface ProjectGroupProps {
+  groupItem: IGroup;
+  onCreate: (groupId?: string) => void;
+  onUpdateGroup: (groupId: string, newName: string) => void;
+}
+
+// 项目分组
+const ProjectGroup = ({ groupItem, onCreate, onUpdateGroup }: ProjectGroupProps) => {
+  const [isEditing, setIsEditing] = useState(false); // 是否正在编辑
+  const [inputValue, setInputValue] = useState(groupItem.name); // 输入框的值
+
+   // 处理编辑分组名称
+  const handleEditGroup = async () => {
+    try {
+      const res = await cmd_invoke("edit_group", { id: groupItem.id, groupName: inputValue });
+      console.log("修改成功", res);
+      setIsEditing(false);
+      // 刷新当前修改的分组名
+      onUpdateGroup(groupItem.id, inputValue);
+    } catch (error) {
+      console.error("修改失败", error);
+    }
+  };
+
+  // 处理输入框失焦
+  const handleBlur = () => {
+    handleEditGroup();
+  };
+
+  // 处理按下回车
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleEditGroup();
+    }
+  };
+
+  // 点击新增项目，调用父组件方法
   const onCreatePro = () => {
     onCreate(groupItem.id);
   };
@@ -17,10 +53,25 @@ const ProjectGroup = ({ groupItem, onCreate }: { groupItem: IGroup; onCreate: (g
     <>
       <div key={groupItem.id} className={styles.group}>
         <div className={styles.groupHeader}>
-          <div>
-            {groupItem.name}
-            <EditOutlined className={styles.editIcon} />
-          </div>
+        <div>
+          {isEditing ? (
+            <Input
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              onBlur={handleBlur}
+              onKeyDown={handleKeyDown}
+              autoFocus
+            />
+          ) : (
+            <>
+              {groupItem.name}
+              <EditOutlined
+                className={styles.editIcon}
+                onClick={() => setIsEditing(true)}
+              />
+            </>
+          )}
+        </div>
           <Button color="primary" variant="link" onClick={onCreatePro}>
             新增项目
           </Button>
