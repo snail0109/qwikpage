a591900732b8ac03173f28c4dfca58f767b453dc
殷艳 <yin.yan01@iwhalecloud.com>
2025-03-05 18:13:15 +0800

    feat:接口模块-列表（50%）

--- frontend/editor/src/layout/components/Menu/Components/ApiList/ApiList.tsx
+++ frontend/editor/src/layout/components/Menu/Components/ApiList/ApiList.tsx
@@ -1,13 +1,52 @@
-import { useRef } from 'react';
-import { Button, Flex, List } from 'antd';
-import { PlusOutlined, SettingOutlined } from '@ant-design/icons';
-import SettingModal from '@/components/ApiConfig/components/SettingModal';
-import { ApiType } from '@/packages/types';
-import { usePageStore } from '@/stores/pageStore';
-import InterceptorModal from '@/components/ApiConfig/components/InterceptorModal';
+import { useRef } from "react";
+import { Button, Table, List, Divider } from "antd";
+import { PlusOutlined, SettingOutlined } from "@ant-design/icons";
+import type { TableProps } from "antd";
+import SettingModal from "@/components/ApiConfig/components/SettingModal";
+import { ApiType } from "@/packages/types";
+import { usePageStore } from "@/stores/pageStore";
+import InterceptorModal from "@/components/ApiConfig/components/InterceptorModal";
+import styles from "./index.module.less";
+
 export default () => {
   const modalRef = useRef<{ showModal: (data?: any) => void }>();
   const interceptorRef = useRef<{ showModal: (data?: any) => void }>();
+
+  const columns: TableProps<ApiType>["columns"] = [
+    {
+      title: "名称",
+      dataIndex: "name",
+      key: "name",
+      render: (_, row) => (
+        <div className={styles.iconCol}>
+          {row.method}
+          {row.name}
+        </div>
+      ),
+    },
+    {
+      title: "URL",
+      dataIndex: "stgApi",
+      key: "stgApi",
+    },
+    {
+      title: "操作",
+      key: "action",
+      width: 120,
+      align: "center",
+      render: (event, row) => (
+        <div style={{ display: 'flex' }}>
+          <Button type="link" onClick={() => handleEdit(event, row)}>
+            修改
+          </Button>
+          <Button type="link" onClick={() => handleRemove(event, row.id)}>
+            删除
+          </Button>
+        </div>
+      ),
+    },
+  ];
+
   // 页面组件
   const { apis, removeApi } = usePageStore((state) => ({
     apis: state.page.pageData.apis,
@@ -38,28 +77,17 @@ export default () => {
 
   return (
     <>
-      <Flex justify="space-between" align="center" style={{ borderBottom: '1px solid var(--mars-theme-card-border-color)' }}>
+      <div className={styles.apiConfigHeader}>
         <Button type="link" icon={<PlusOutlined />} onClick={() => handleAdd()}>
           新增
         </Button>
+        <Divider type="vertical" />
         <Button type="link" icon={<SettingOutlined />} onClick={() => handleInterceptors()}>
           全局拦截器
         </Button>
-      </Flex>
-      <List
-        style={{ height: 'calc(100vh - 150px)', overflowY: 'auto' }}
-        itemLayout="horizontal"
-        dataSource={Object.values(apis)}
-        renderItem={(item) => {
-          return (
-            <List.Item
-              actions={[<a onClick={(event) => handleEdit(event, item)}>修改</a>, <a onClick={(event) => handleRemove(event, item.id)}>删除</a>]}
-            >
-              <List.Item.Meta title={`${item.method} ${item.name}`} description={item.url} />
-            </List.Item>
-          );
-        }}
-      />
+      </div>
+
+      <Table<ApiType> size="small" columns={columns} dataSource={Object.values(apis)} />
       {/* 接口设置 */}
       <SettingModal ref={modalRef}></SettingModal>
       {/* 拦截器设置 */}
