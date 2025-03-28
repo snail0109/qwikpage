b2e31132dc020666677364b75860dd7baa29d2e8
张玉梅 <zhang.yumei4@iwhalecloud.com>
2025-03-28 16:53:55 +0800

    feat: 导出的脚手架内加入启动脚本

--- backend/code-vue/src/constant.rs
+++ backend/code-vue/src/constant.rs
@@ -131,13 +131,90 @@ app.use(router);
 app.mount('#app');
 "#;
 
+pub const START_SH_CONFIG: &str = r#"
+#!/bin/bash
+
+# 检查 Node.js 版本，要求最低版本为 18
+echo "检查 Node.js 版本"
+node_version=$(node -v)
+if [[ ! $node_version =~ ^v1[89] ]] && [[ ! $node_version =~ ^v[2-9][0-9] ]]; then
+    echo "Node.js 版本必须 >= 18，当前版本为 $node_version"
+    exit 1
+fi
+
+# 检查 npm 是否安装
+echo "检查 npm"
+if ! command -v npm &> /dev/null; then
+    echo "npm 未安装，请安装 npm"
+    exit 1
+else
+    echo "安装依赖"
+    if npm install; then
+        echo "npm install 成功"
+    else
+        echo "npm install 失败"
+        exit 1
+    fi
+fi
+
+# 启动项目
+echo "启动项目"
+if npm run dev; then
+    echo "项目启动成功"
+else
+    echo "项目启动失败"
+    exit 1
+fi
+
+"#;
+
+pub const START_WIN_CONFIG: &str = r#"
+@echo off
+
+REM 检查 Node.js 版本，要求最低版本为 18
+echo 检查 Node.js 版本
+for /f "tokens=2 delims=v" %%i in ('node -v') do set node_version=%%i
+for /f "tokens=1 delims=." %%i in ("%node_version%") do set major_version=%%i
+
+if %major_version% lss 18 (
+    echo Node.js 版本必须 >= 18，当前版本为 v%node_version%
+    exit /b 1
+)
+REM 检查 npm 是否安装
+echo 检查 npm
+where npm >nul 2>nul
+if %errorlevel% neq 0 (
+    echo npm 未安装，请安装 npm
+    exit /b 1
+) else (
+    echo 安装依赖
+    npm install
+    if %errorlevel% neq 0 (
+        echo npm install 失败
+        exit /b 1
+    ) else (
+        echo npm install 成功
+    )
+)
+REM 启动项目
+echo 启动项目
+npm run dev
+if %errorlevel% neq 0 (
+    echo 项目启动失败
+    exit /b 1
+) else (
+    echo 项目启动成功
+)
+
+"#;
+
 #[derive(Debug)]
 pub struct FileTemplate {
     pub filename: String,
     pub content: String,
 }
 
-pub fn template_files() -> [FileTemplate; 8] {
+pub fn template_files() -> [FileTemplate; 10] {
     [
         FileTemplate {
             filename: String::from("vite.config.ts"),
@@ -171,6 +248,14 @@ pub fn template_files() -> [FileTemplate; 8] {
         FileTemplate {
             filename: String::from("src/main.ts"),
             content: String::from(MAIN),
+        },
+        FileTemplate {
+          filename: String::from("start"),
+          content: String::from(START_SH_CONFIG),
+        },
+        FileTemplate {
+          filename: String::from("start.cmd"),
+          content: String::from(START_WIN_CONFIG),
         },
     ]
 }
