c9a5b72431148852951292b16724d7ad6b6c7390
dingxy <>
2025-04-02 14:07:37 +0800

    feat: 添加tauri-http 插件

--- backend/tauri/src/lib.rs
+++ backend/tauri/src/lib.rs
@@ -94,6 +94,7 @@ pub fn run() {
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .setup(|app| {
