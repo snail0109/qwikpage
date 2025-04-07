68e673448ea9fef9b8966f9d8a0aaa0cec40137c
yinyan <yin.yan01@iwhalecloud.com>
2025-04-07 15:22:11 +0800

    功能有误，暂时删除代码

--- frontend/editor/src/components/SetterRender/SetterRender.tsx
+++ frontend/editor/src/components/SetterRender/SetterRender.tsx
@@ -1,7 +1,7 @@
-import React, { memo, useEffect } from 'react';
+import React, { memo } from 'react';
 import { Form, Input, InputNumber, Radio, Select, Switch, Slider, FormInstance, Tooltip, Popover } from 'antd';
 import * as icons from '@qwikpage/icons';
-import { QuestionCircleOutlined, CaretDownOutlined } from '@ant-design/icons';
+import { CaretDownOutlined } from '@ant-design/icons';
 import { SchemaType } from '@/packages/types';
 import MColorPicker from '../ColorPicker';
 import VariableBindInput from '../VariableBind/VariableBind';
@@ -34,55 +34,7 @@ const SetterRender = memo(({ attrs, form }: IAttrs) => {
 
   const elementId = selectedElement?.id;
   const formItemId = selectedElement?.id ? elementsMap[selectedElement.id]?.config?.props?.formItem?.name : undefined;
-
-  useEffect(() => {
-    if (!formItemId || !elementId) return;
-
-    // 准备要设置的表单值
-    const formValues = { formItem: {} };
-    let hasChanges = false;
-
-    // 获取当前元素的表单项配置
-    const formItemConfig = elementsMap[elementId]?.config?.props?.formItem;
-
-    // 处理labelCol配置
-    if (formItemConfig?.labelCol?.span !== undefined) {
-      formValues.formItem.labelCol = { span: formItemConfig.labelCol.span };
-      hasChanges = true;
-    } else {
-      // 如果当前元素没有配置，尝试获取父元素的配置
-      const parentId = elementsMap[elementId]?.parentId;
-      if (parentId && elementsMap[parentId]) {
-        const parentElementLabelCol = elementsMap[parentId].config.props.labelCol;
-        if (parentElementLabelCol?.span !== undefined) {
-          formValues.formItem.labelCol = { span: parentElementLabelCol.span };
-          hasChanges = true;
-        }
-      }
-    }
-
-    // 处理wrapperCol配置
-    if (formItemConfig?.wrapperCol?.span !== undefined) {
-      formValues.formItem.wrapperCol = { span: formItemConfig.wrapperCol.span };
-      hasChanges = true;
-    } else {
-      // 如果当前元素没有配置，尝试获取父元素的配置
-      const parentId = elementsMap[elementId]?.parentId;
-      if (parentId && elementsMap[parentId]) {
-        const parentElementWrapperCol = elementsMap[parentId].config.props.wrapperCol;
-        if (parentElementWrapperCol?.span !== undefined) {
-          formValues.formItem.wrapperCol = { span: parentElementWrapperCol.span };
-          hasChanges = true;
-        }
-      }
-    }
-
-    // 只有当有值需要设置时才调用setFieldsValue，避免不必要的渲染
-    if (hasChanges) {
-      form.setFieldsValue(formValues);
-    }
-  }, [formItemId, elementId, elementsMap, form]);
-
+   
   if (attrs.length === 0) return <></>;
   // console.log(attrs)
 
@@ -121,20 +73,6 @@ const SetterRender = memo(({ attrs, form }: IAttrs) => {
 
         if (item.type == 'Title') {
           return null;
-          // return (
-          //   <h2 className={styles.title} key={key}>
-          //     <span style={{ marginRight: 10 }}>{item.label}</span>
-          //     {/* 标题增加提示信息 */}
-          //     {item.tooltip ? <Tooltip title={item.tooltip}>{<QuestionCircleOutlined />}</Tooltip> : null}
-
-          //     {/* 标题增加跳转链接 */}
-          //     {item.link ? (
-          //       <a href={item.link.url} target="_blank" style={{ fontSize: 12 }}>
-          //         {item.link.label}
-          //       </a>
-          //     ) : null}
-          //   </h2>
-          // );
         } else if (item.type == 'Input') {
           FormControl = <Input {...item.props} />;
         } else if (item.type === 'InputPx') {
