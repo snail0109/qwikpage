3def2dbd37e2ad8fee07bb9f12648de6cd6e089e
dingxy <>
2025-04-10 20:18:35 +0800

    feat: 预览资源更新

---
+++ backend/tauri/assets/admin/assets/Divider-D7ayXBFT.js
@@ -0,0 +1,1 @@
+import{r,j as o}from"./index-ClmqYIeJ.js";import{af as a}from"./Page-3T-yq0dr.js";import"./index-BYsoRLRM.js";import"./index-DJMKYpA5.js";const p=({config:e},s)=>{const[i,t]=r.useState(!0);return r.useImperativeHandle(s,()=>({show(){t(!0)},hide(){t(!1)}})),i&&o.jsx(a,{style:e.style,...e.props,children:e.props.text})},n=r.forwardRef(p);export{n as default};
