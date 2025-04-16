512c4bf94b2a69df0f0be9ba9185a0a1b2f83b4a
yinyan <yin.yan01@iwhalecloud.com>
2025-04-16 18:03:43 +0800

    fix:带icon的button样式

--- frontend/editor/src/components/icons/QIcon.tsx
+++ frontend/editor/src/components/icons/QIcon.tsx
@@ -3,27 +3,36 @@ import * as icons from '@qwikpage/icons';
 import { renderIconDefinitionToSVGElement } from '@qwikpage/icons/es/helpers';
 
 const iconsList: { [key: string]: any } = icons;
-const QIcon = ({ name, style = {} }: { name: string; style?: any }) => {
-    const [iconSvg, setIconSvg] = useState<string>('');
 
-    useEffect(() => {
-        const svgHTMLString = renderIconDefinitionToSVGElement(iconsList[name], {
-            extraSVGAttrs: { width: '1em', height: '1em', fill: 'currentColor' },
-        })
-        setIconSvg(svgHTMLString);
-    }, [name]);
+interface QIconProps {
+  name: string;
+  style?: React.CSSProperties;
+  onClick?: () => void;
+  className?: string;
+}
 
-    return iconSvg ? (
-        <span
-            className='anticon'
-            style={{
-                fontSize: '18px',
-                verticalAlign: 'middle',
-                ...style,
-            }}
-            dangerouslySetInnerHTML={{ __html: iconSvg }}
-        />
-    ) : null
+const QIcon = ({ name, style = {}, onClick, className }: QIconProps) => {
+  const [iconSvg, setIconSvg] = useState<string>('');
+
+  useEffect(() => {
+    const svgHTMLString = renderIconDefinitionToSVGElement(iconsList[name], {
+      extraSVGAttrs: { width: '1em', height: '1em', fill: 'currentColor' },
+    });
+    setIconSvg(svgHTMLString);
+  }, [name]);
+
+  return iconSvg ? (
+    <span
+      className={`anticon ${className || ''}`}
+      style={{
+        fontSize: '18px',
+        display: 'flex',
+        ...style,
+      }}
+      onClick={onClick}
+      dangerouslySetInnerHTML={{ __html: iconSvg }}
+    />
+  ) : null;
 };
 
 export default QIcon;
