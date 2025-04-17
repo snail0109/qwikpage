afe87efadbc5be3af20edd2d58565518f958db2c
dingxy <>
2025-04-17 18:06:12 +0800

    fix：函数类名

--- frontend/editor/src/components/StyleConfig/transform.ts
+++ frontend/editor/src/components/StyleConfig/transform.ts
@@ -1,7 +1,7 @@
 import cssParser from 'css';
 /**
  * css 转换为 object
- * .marsview { font-size: 12px } 生成：{ marsview:{ fontSize: '12px' } }
+ * .qwikpage { font-size: 12px } 生成：{ qwikpage:{ fontSize: '12px' } }
  * @param rules
  * @param result
  */
@@ -65,7 +65,7 @@ export function transform(inputCssText: string) {
   let bootstrapWithCssClass = false;
   if (inputCssText.indexOf('{') === -1) {
     bootstrapWithCssClass = true;
-    inputCssText = `.marsview { ${inputCssText} }`;
+    inputCssText = `.qwikpage { ${inputCssText} }`;
   }
   const css = cssParser.parse(inputCssText);
   let result: any = {};
@@ -73,7 +73,7 @@ export function transform(inputCssText: string) {
 
   // Don't expose the implementation detail of our wrapped css class.
   if (bootstrapWithCssClass) {
-    result = result.marsview;
+    result = result.qwikpage;
   }
 
   return result;
