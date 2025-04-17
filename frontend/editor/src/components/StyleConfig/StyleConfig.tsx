935d1c1937eba2552540efcbd45ef66d7de44899
dingxy <>
2025-04-17 18:04:18 +0800

    fix：样式页面默认类名使用组件类型

--- frontend/editor/src/components/StyleConfig/StyleConfig.tsx
+++ frontend/editor/src/components/StyleConfig/StyleConfig.tsx
@@ -35,12 +35,12 @@ const StyleConfig = () => {
     form.resetFields();
     // 填充页面样式
     if (!state.selectedElement) {
-      form.setFieldValue('scopeCss', state.page.pageData.config.scopeCss || '/* 请在此处添加样式*/\n.marsview{\n\n}');
+      form.setFieldValue('scopeCss', state.page.pageData.config.scopeCss || '/* 请在此处添加样式*/\n.qwikpage{\n\n}');
       form.setFieldValue('scopeStyle', state.page.pageData.config.scopeStyle);
     } else {
       // 填充组件样式
       const config = state.page.pageData.elementsMap[state.selectedElement.id]?.config || {};
-      form.setFieldValue('scopeCss', config.scopeCss || '/* 请在此处添加样式*/\n.marsview{\n\n}');
+      form.setFieldValue('scopeCss', config.scopeCss || `/* 请在{}内添加自定义样式*/\n.${state.selectedElement.type}{\n\n}`);
       form.setFieldValue('scopeStyle', config.scopeStyle || config.style);
       // 无实际作用，主要用于触发组件更新
       setRender(!isRender);
