55990b45f3a41489fad4f14426edf58fac3cf1b4
Zhangym <zhang.yumei4@iwhalecloud.com>
2025-04-17 14:19:43 +0800

    feat: 编辑态定义projectStore

--- frontend/editor/src/stores/projectStore.ts
+++ frontend/editor/src/stores/projectStore.ts
@@ -1,57 +1,69 @@
 import { create } from 'zustand';
-import { IMenuItem } from '@/types/index';
+import { produce } from 'immer';
+import { PageVariable } from '@/packages/types';
 
-export interface ProjectInfo {
-  id?: string;
-  footer?: number;
-  layout?: number;
-  logo?: string;
-  menuMode: 'inline' | 'horizontal' | 'vertical';
-  menuThemeColor?: string;
-  name: string;
-  systemThemeColor?: string;
-  breadcrumb?: number;
-  tag?: number;
-}
 
-export interface PageState {
-  projectInfo: ProjectInfo;
-  collapsed: boolean;
-  menuTree: any[];
-  buttons: any[];
-  pageMap: { [key: string]: IMenuItem };
-  menuMap: { [key: string]: IMenuItem };
+export interface ProjectState {
+  variables: PageVariable[];
+  variableData: { [key: string]: any };
 }
 
 export interface ProjectAction {
-  setProjectInfo: (payload: any) => void;
-  updateCollapsed: () => void;
+  setVariables: (payload: PageVariable[]) => void;
+  addVariable: (payload: PageVariable) => void;
+  editVariable: (payload: PageVariable) => void;
+  removeVariable: (name: string) => void;
+  setVariableData: (payload: any) => void;
 }
 
-export const useProjectStore = create<PageState & ProjectAction>((set, get) => ({
-  projectInfo: {
-    name: '',
-    menuMode: 'inline',
+export const useProjectStore = create<ProjectState & ProjectAction>((set, get) => ({
+  // 项目变量定义列表
+  variables: [],
+  // 项目变量数据
+  variableData: {},
+  // 初始化变量列表
+  setVariables(payload: PageVariable[]) {
+    set(
+      produce((state) => {
+        state.variables = payload;
+      }),
+    );
   },
-  collapsed: false,
-  menuTree: [],
-  buttons: [],
-  pageMap: {},
-  menuMap: {},
-  setProjectInfo: ({ projectInfo, menuTree, buttons, pageMap, menuMap }: any) => {
-    set({
-      projectInfo,
-      menuTree,
-      buttons,
-      pageMap,
-      menuMap,
-    });
+  // 添加变量
+  addVariable(payload: PageVariable) {
+    set(
+      produce((state) => {
+        state.variables.push(payload);
+      }),
+    );
   },
-  updateCollapsed: () => {
-    set((state) => {
-      return {
-        collapsed: !state.collapsed,
-      };
-    });
+  // 更新变量
+  editVariable(payload: PageVariable) {
+    set(
+      produce((state) => {
+        const index = state.variables.findIndex((item: PageVariable) => item.name == payload.name);
+        if (index > -1) {
+          state.variableData[payload.name] = payload.defaultValue;
+          state.variables[index] = payload;
+        }
+      }),
+    );
+  },
+  // 删除变量
+  removeVariable(name: string) {
+    set(
+      produce((state) => {
+        state.isEdit = true; // 标记为编辑状态
+        state.variables = state.variables.filter((item: PageVariable) => item.name !== name);
+      }),
+    );
+  },
+  // 设置变量数据
+  setVariableData({ name, value }: any) {
+    set(
+      produce((state) => {
+        state.variableData[name] = value;
+      }),
+    );
   },
 }));
