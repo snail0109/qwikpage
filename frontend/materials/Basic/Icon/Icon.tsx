ce653991892e299ab224210bfcff22c7c9f2b8b4
yinyan <yin.yan01@iwhalecloud.com>
2025-04-25 15:51:00 +0800

    fix:修复icon预览

--- frontend/materials/Basic/Icon/Icon.tsx
+++ frontend/materials/Basic/Icon/Icon.tsx
@@ -1,12 +1,7 @@
 import React, { useState, useImperativeHandle, forwardRef } from 'react';
-import * as Icons from '@ant-design/icons';
+import QIcon from '@materials/components/icons/QIcon';
 import { ComponentType } from '@materials/types';
-/**
- *
- * @param props 组件本身属性
- * @param style 组件样式
- * @returns
- */
+
 const MIcon = (
   {
     config,
@@ -15,38 +10,31 @@ const MIcon = (
     icon: string;
     style?: React.CSSProperties;
   }>,
-  ref: any,
+  ref: any
 ) => {
   const [visible, setVisible] = useState(true);
+  
   // 对外暴露方法
-  useImperativeHandle(ref, () => {
-    return {
-      show() {
-        setVisible(true);
-      },
-      hide() {
-        setVisible(false);
-      },
-    };
-  });
+  useImperativeHandle(ref, () => ({
+    show() {
+      setVisible(true);
+    },
+    hide() {
+      setVisible(false);
+    },
+  }));
 
   const handleClick = () => {
     onClick?.();
   };
 
-  const iconsList: { [key: string]: any } = Icons;
-
-   // 获取icon名称，如果没有则使用默认值GithubOutlined
-   const iconName = config.props?.icon || 'GithubOutlined';
-   const IconComponent = iconsList[iconName] || iconsList['GithubOutlined'];
-   
   return (
-    visible &&
-    React.createElement(IconComponent, {
-      style: config.style,
-      ...config.props,
-      onClick: handleClick,
-    })
+    visible && (
+      <span onClick={handleClick}>
+        <QIcon style={config.style} {...config.props} name={config.props.icon || 'GithubOutlined'} />
+      </span>
+    )
   );
 };
+
 export default forwardRef(MIcon);
